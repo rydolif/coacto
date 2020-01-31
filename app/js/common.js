385564b0428ec59aa59e4766d2b583225b9d29f9
Mykola Polishchuk <rudolifrudolif@gmail.com>
2020-01-31 12:55:01 +0200

    start commit

---
+++ app/js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+
+//-------------------------reviews slider---------------------------------------
+    var swiper = new Swiper('.reviews__slider', {
+      spaceBetween: 25,
+      navigation: {
+        nextEl: '.reviews__next',
+        prevEl: '.reviews__prev',
+      },
+    });
+
+});
+
